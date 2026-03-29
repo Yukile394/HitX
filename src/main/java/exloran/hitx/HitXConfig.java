f39ce99559bc6c9ab91c4693938cec353e89f9be
Yukile394 <muhamedmuhet@gmail.com>
2026-03-29 20:55:55 +0300

    Add HUD position configuration options
    Added HUD position configuration options for X and Y percentages.

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -7,6 +7,14 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
 
+    @ConfigEntry.Category("hud")
+    @ConfigEntry.Gui.Tooltip(count = 1)
+    public int hudXPercent = 75; // %75 sağda (Kırmızı alan)
+
+    @ConfigEntry.Category("hud")
+    @ConfigEntry.Gui.Tooltip(count = 1)
+    public int hudYPercent = 45; // %45 yukarda (Kırmızı alan)
+
     @ConfigEntry.BoundedDiscrete(min = 4, max = 20)
     public int duration = 8;
 
@@ -15,7 +23,7 @@ public class HitXConfig implements ConfigData {
 
     @ConfigEntry.ColorPicker
     public int red = 255;
-
+    
     public int green = 255;
     public int blue = 255;
 
