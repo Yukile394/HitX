89a703a8b2517e9c5d28832f90d22d98d932f7ce
Yukile394 <muhamedmuhet@gmail.com>
2026-04-01 14:15:59 +0300

    Refactor HitXConfig to implement ClientModInitializer

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -5,30 +5,25 @@ import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
-public class HitXConfig implements ConfigData {
+public class HitXConfig implements ClientModInitializer {
+    // ClientModInitializer eklemesi gerekiyorsa ekle yoksa silebilirsin.
+    @ConfigEntry.Category("hud")
+    public int hudX = 78;
 
     @ConfigEntry.Category("hud")
-    @ConfigEntry.Gui.Tooltip(count = 1)
-    public int hudX = 78; // HUD Yatay Konum (%)
-
-    @ConfigEntry.Category("hud")
-    @ConfigEntry.Gui.Tooltip(count = 1)
-    public int hudY = 40; // HUD Dikey Konum (%)
+    public int hudY = 40;
 
     @ConfigEntry.Category("hud")
     @ConfigEntry.BoundedDiscrete(min = 50, max = 200)
-    public int hudScale = 100; // HUD Boyutu (%)
+    public int hudScale = 100;
 
     @ConfigEntry.Category("hud")
-    @ConfigEntry.Gui.Tooltip(count = 1)
-    public boolean particleOn = true; // EKLENDİ: Partiküller açık/kapalı
+    public boolean particleOn = true;
 
     @ConfigEntry.Gui.CollapsibleObject
     public Visuals visuals = new Visuals();
 
     public static class Visuals {
-        @ConfigEntry.ColorPicker
-        public int barColor = 0xFF0000;
-        public boolean sabitBar = true; // Kafada sabit dursun mu?
+        public boolean sabitBar = true;
     }
 }
