4b16292676bb8c3ea038147f7012d0783933fbb0
Yukile394 <muhamedmuhet@gmail.com>
2026-04-08 16:00:24 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -2,7 +2,6 @@ package exloran.hitx;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
@@ -10,12 +9,4 @@ public class HitXConfig implements ConfigData {
     public int hudX = 50;
     public int hudY = 50;
     public int hudScale = 100;
-
-    @ConfigEntry.Gui.CollapsibleObject
-    public Visuals visuals = new Visuals();
-
-    public static class Visuals {
-        public boolean sabitBar = false;
-        public int barColor = 0xFFFFFFFF;
-    }
 }
