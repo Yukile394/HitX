26c4f04bc1cf17489666aa45a90b2d1516e35ff6
Yukile394 <muhamedmuhet@gmail.com>
2026-04-08 15:38:46 +0300

    Update ModMenuIntegration.java

--- src/main/java/exloran/hitx/ModMenuIntegration.java
+++ src/main/java/exloran/hitx/ModMenuIntegration.java
@@ -1,14 +1,12 @@
-package com.exloran.hitx;
+package exloran.hitx;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
-
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent ->
-                AutoConfig.getConfigScreen(HitXConfig.class, parent).get();
+        return parent -> AutoConfig.getConfigScreen(HitXConfig.class, parent).get();
     }
 }
