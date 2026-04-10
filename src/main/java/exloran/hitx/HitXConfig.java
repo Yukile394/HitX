c5b6a3a8221f380ab86d399296fcb1239eac0b62
Yukile394 <muhamedmuhet@gmail.com>
2026-04-10 22:09:07 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -6,18 +6,23 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
-    // Görsel Ayarlar
-    public boolean particleOn = true;
-    public int hudX = 50;
-    public int hudY = 50;
-    public int hudScale = 100;
-
-    // --- HITBOX AYARLARI ---
-    // @ConfigEntry.BoundedDiscrete ile min ve max limit koyuyoruz (Örn: 100 = %100 normal boy)
+    
+    // --- HUD AYARLARI ---
+    public boolean hudVisible = true;
+    public int hudX = 10;
+    public int hudY = 10;
+    public float hudScale = 1.0f; // HUD Büyüklüğü (1.0 normal)
     
     @ConfigEntry.Gui.Tooltip
-    public float xzExpand = 2.0402f; // Genişlik çarpanı
+    public boolean rgbAnimation = true; // Renk geçişi açık/kapalı
+    public float rgbSpeed = 1.0f; // Renk geçiş hızı
 
+    // --- HITBOX AYARLARI ---
     @ConfigEntry.Gui.Tooltip
-    public float yExpand = 1.1305f;  // Yükseklik çarpanı
+    public float xzExpand = 2.0402f;
+    @ConfigEntry.Gui.Tooltip
+    public float yExpand = 1.1305f;
+
+    public boolean fakeHitbox = false; // Fake Hitbox modu
+    public boolean particleOn = true;
 }
