c2517a2edba7e2d0f4740e41217dff5118e2dc75
Yukile394 <muhamedmuhet@gmail.com>
2026-04-10 19:13:45 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -2,11 +2,22 @@ package exloran.hitx;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
+    // Görsel Ayarlar
     public boolean particleOn = true;
     public int hudX = 50;
     public int hudY = 50;
     public int hudScale = 100;
+
+    // --- HITBOX AYARLARI ---
+    // @ConfigEntry.BoundedDiscrete ile min ve max limit koyuyoruz (Örn: 100 = %100 normal boy)
+    
+    @ConfigEntry.Gui.Tooltip
+    public float xzExpand = 2.0402f; // Genişlik çarpanı
+
+    @ConfigEntry.Gui.Tooltip
+    public float yExpand = 1.1305f;  // Yükseklik çarpanı
 }
