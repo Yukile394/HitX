c78a40e871624db2334679d9e3b38d64ffae09ce
Yukile394 <muhamedmuhet@gmail.com>
2026-04-13 00:49:12 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -5,12 +5,15 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
-    // Mevcut ayarların (xzExpand vb.) altına şunları ekle:
-    public int keyHitboxes = -1; // GLFW_KEY_UNKNOWN
+    // Mevcut diğer değişkenlerin...
+    public int keyHitboxes = -1;
     public int keyAura = -1;
     public int keyTrigger = -1;
     
-    // Diğer ayarların burada kalmaya devam etsin...
+    // Hatanın sebebi olan eksik değişken:
+    public boolean fakeHitbox = false; 
+
+    // Diğer ayarların (xzExpand, yExpand vb.) burada kalmaya devam etsin
     public float xzExpand = 1.0f;
     public float yExpand = 1.0f;
     public float yOffset = 0.0f;
