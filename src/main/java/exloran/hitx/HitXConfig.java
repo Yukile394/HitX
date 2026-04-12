75b3d6f7ba95bfba7405e990ee4ccd350d14b89e
Yukile394 <muhamedmuhet@gmail.com>
2026-04-13 00:46:10 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -2,29 +2,16 @@ package exloran.hitx;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
-
-    // HUD
-    public boolean hudVisible   = true;
-    public int     hudX         = 10;
-    public int     hudY         = 10;
-    public float   hudScale     = 1.0f;
-    public boolean rgbAnimation = true;
-    public float   rgbSpeed     = 1.0f;
-
-    // HitBox
-    @ConfigEntry.Gui.Tooltip
-    public float xzExpand = 2.0402f;  // XZ genişleme çarpanı
-
-    @ConfigEntry.Gui.Tooltip
-    public float yExpand  = 1.1305f;  // Y yükseklik çarpanı
-
-    @ConfigEntry.Gui.Tooltip
-    public float yOffset  = 0.0f;     // Yukarı/aşağı kaydırma
-
-    public boolean fakeHitbox = false;
-    public boolean particleOn = true;
+    // Mevcut ayarların (xzExpand vb.) altına şunları ekle:
+    public int keyHitboxes = -1; // GLFW_KEY_UNKNOWN
+    public int keyAura = -1;
+    public int keyTrigger = -1;
+    
+    // Diğer ayarların burada kalmaya devam etsin...
+    public float xzExpand = 1.0f;
+    public float yExpand = 1.0f;
+    public float yOffset = 0.0f;
 }
