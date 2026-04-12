b9770df783a44e7df08ba670ced5b550aa5b841b
Yukile394 <muhamedmuhet@gmail.com>
2026-04-12 23:21:58 +0300

    Update HitXConfig.java

--- src/main/java/exloran/hitx/HitXConfig.java
+++ src/main/java/exloran/hitx/HitXConfig.java
@@ -6,27 +6,25 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "hitx")
 public class HitXConfig implements ConfigData {
-    
-    // --- HUD AYARLARI ---
-    public boolean hudVisible = true;
-    public int hudX = 10;
-    public int hudY = 10;
-    public float hudScale = 1.0f; // HUD Büyüklüğü (1.0 normal)
-    
-    @ConfigEntry.Gui.Tooltip
-    public boolean rgbAnimation = true; // Renk geçişi açık/kapalı
-    public float rgbSpeed = 1.0f; // Renk geçiş hızı
 
-    // --- HITBOX AYARLARI ---
+    // HUD
+    public boolean hudVisible   = true;
+    public int     hudX         = 10;
+    public int     hudY         = 10;
+    public float   hudScale     = 1.0f;
+    public boolean rgbAnimation = true;
+    public float   rgbSpeed     = 1.0f;
+
+    // HitBox
     @ConfigEntry.Gui.Tooltip
-    public float xzExpand = 2.0402f;
-    
-    @ConfigEntry.Gui.Tooltip
-    public float yExpand = 1.1305f;
+    public float xzExpand = 2.0402f;  // XZ genişleme çarpanı
 
     @ConfigEntry.Gui.Tooltip
-    public float yOffset = 0.0f; // Hitbox Yukarı/Aşağı kaydırma ayarı
+    public float yExpand  = 1.1305f;  // Y yükseklik çarpanı
 
-    public boolean fakeHitbox = false; // Fake Hitbox modu
+    @ConfigEntry.Gui.Tooltip
+    public float yOffset  = 0.0f;     // Yukarı/aşağı kaydırma
+
+    public boolean fakeHitbox = false;
     public boolean particleOn = true;
 }
