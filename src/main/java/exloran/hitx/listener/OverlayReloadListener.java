11884bc572ef26bf891a48db459a44bcf2d2aa81
Yukile394 <muhamedmuhet@gmail.com>
2026-04-15 23:44:57 +0300

    Create OverlayReloadListener.java

---
+++ src/main/java/exloran/hitx/listener/OverlayReloadListener.java
@@ -0,0 +1,21 @@
+package exloran.hitx.listener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface OverlayReloadListener {
+    List<OverlayReloadListener> listeners = new ArrayList<>();
+    
+    void onOverlayReload();
+
+    static void register(OverlayReloadListener listener) {
+        listeners.add(listener);
+    }
+
+    static void callEvent() {
+        for (OverlayReloadListener listener : listeners) {
+            listener.onOverlayReload();
+        }
+    }
+}
+
